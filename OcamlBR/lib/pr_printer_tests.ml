fda5b5e7b5b19e1c7522b37ac10a943e8d051b3f
sofyak0zyreva <k81sofia@gmail.com>
2025-01-20 02:04:11 +0300

    docs: tests for pretty-printing
    Signed-off-by: sofyak0zyreva <k81sofia@gmail.com>

--- OcamlBR/lib/pr_printer_tests.ml
+++ OcamlBR/lib/pr_printer_tests.ml
@@ -365,3 +365,236 @@ let%expect_test _ =
      ]
   |}]
 ;;
+
+let%expect_test _ =
+  parse
+    "type d = { aa: int }\n\
+    \      type old = { aa: int list -> int ; bb: string option }\n\
+    \      type new = { aa: int; bb: bool }\n\
+    \      let f = fun x -> x\n\
+    \      let g x = \n\
+    \        (match x with\n\
+    \        | [] -> 0\n\
+    \        | hd :: snd :: tl -> hd\n\
+    \        | h::tl -> 1)\n\
+    \      let m = { aa = g [5; 13; 92] ; bb = f (not true) } \n\
+    \      let p : old = { aa = g ; bb = Some \"hello\" }\n\
+    \      type k = { s : old; z : new }\n\
+    \      let i = { s = p; z = m }\n\
+    \      let f x = x.aa\n\
+    \      let h (x : old) = x.aa\n\
+    \      let r = f m + p.aa [1; 3; 5; 7]";
+  [%expect
+    {|
+      type d = { aa : int } ;;
+      type old = { aa : int list -> int ; bb : (string) option } ;;
+      type new = { aa : int ; bb : bool } ;;
+      let  f = (fun x -> x) ;;
+      let  g = (fun x -> match x with | [] -> 0 | hd :: snd :: tl -> hd | h :: tl -> 1) ;;
+      let  m = { aa = g ([5; 13; 92]) ; bb = f (not (true)) } ;;
+      let  p : old = { aa = g ; bb = (Some "hello") } ;;
+      type k = { s : old ; z : new } ;;
+      let  i = { s = p ; z = m } ;;
+      let  f = (fun x -> x.aa) ;;
+      let  h = (fun (x : old) -> x.aa) ;;
+      let  r = f m + (p.aa) ([1; 3; 5; 7]) ;;
+      [(SType ("d", (Sfield_decl ((Label "aa"), int)), []));
+        (SType ("old", (Sfield_decl ((Label "aa"), int list -> int)),
+           [(Sfield_decl ((Label "bb"), (string) option))]));
+        (SType ("new", (Sfield_decl ((Label "aa"), int)),
+           [(Sfield_decl ((Label "bb"), bool))]));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "f")), None),
+              (Efun (((PVar (Id "x")), None), [], (Evar (Id "x")))))),
+           []));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "g")), None),
+              (Efun (((PVar (Id "x")), None), [],
+                 (Ematch ((Evar (Id "x")), (Ecase ((PList []), (Econst (Int 0)))),
+                    [(Ecase (
+                        (PCons ((PVar (Id "hd")),
+                           (PCons ((PVar (Id "snd")), (PVar (Id "tl")))))),
+                        (Evar (Id "hd"))));
+                      (Ecase ((PCons ((PVar (Id "h")), (PVar (Id "tl")))),
+                         (Econst (Int 1))))
+                      ]
+                    ))
+                 ))
+              )),
+           []));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "m")), None),
+              (Erecord (
+                 (Erecord_field ((Label "aa"),
+                    (Efun_application ((Evar (Id "g")),
+                       (Elist
+                          [(Econst (Int 5)); (Econst (Int 13)); (Econst (Int 92))])
+                       ))
+                    )),
+                 [(Erecord_field ((Label "bb"),
+                     (Efun_application ((Evar (Id "f")),
+                        (Eun_op (Not, (Econst (Bool true))))))
+                     ))
+                   ]
+                 ))
+              )),
+           []));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "p")), (Some old)),
+              (Erecord ((Erecord_field ((Label "aa"), (Evar (Id "g")))),
+                 [(Erecord_field ((Label "bb"),
+                     (Eoption (Some (Econst (String "hello"))))))
+                   ]
+                 ))
+              )),
+           []));
+        (SType ("k", (Sfield_decl ((Label "s"), old)),
+           [(Sfield_decl ((Label "z"), new))]));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "i")), None),
+              (Erecord ((Erecord_field ((Label "s"), (Evar (Id "p")))),
+                 [(Erecord_field ((Label "z"), (Evar (Id "m"))))]))
+              )),
+           []));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "f")), None),
+              (Efun (((PVar (Id "x")), None), [],
+                 (Efield_access ((Evar (Id "x")), (Label "aa")))))
+              )),
+           []));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "h")), None),
+              (Efun (((PVar (Id "x")), (Some old)), [],
+                 (Efield_access ((Evar (Id "x")), (Label "aa")))))
+              )),
+           []));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "r")), None),
+              (Ebin_op (Add, (Efun_application ((Evar (Id "f")), (Evar (Id "m")))),
+                 (Efun_application (
+                    (Efield_access ((Evar (Id "p")), (Label "aa"))),
+                    (Elist
+                       [(Econst (Int 1)); (Econst (Int 3)); (Econst (Int 5));
+                         (Econst (Int 7))])
+                    ))
+                 ))
+              )),
+           []))
+        ]
+   
+  |}]
+;;
+
+let%expect_test _ =
+  parse
+    "\n\
+    \      type inner = { x : int }\n\
+    \      type outer = { x : string; inner : inner }\n\
+    \      let x = { x = \"hello\" ; inner = { x = 34 } }\n\
+    \      let y = if x.inner.x > 100 then print_int x.inner.x else print_endline x.x";
+  [%expect
+    {|
+      type inner = { x : int } ;;
+      type outer = { x : string ; inner : inner } ;;
+      let  x = { x = "hello" ; inner = { x = 34 } } ;;
+      let  y = if x.inner.x > 100 then print_int (x.inner.x) else print_endline (x.x) ;;
+      [(SType ("inner", (Sfield_decl ((Label "x"), int)), []));
+        (SType ("outer", (Sfield_decl ((Label "x"), string)),
+           [(Sfield_decl ((Label "inner"), inner))]));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "x")), None),
+              (Erecord ((Erecord_field ((Label "x"), (Econst (String "hello")))),
+                 [(Erecord_field ((Label "inner"),
+                     (Erecord ((Erecord_field ((Label "x"), (Econst (Int 34)))),
+                        []))
+                     ))
+                   ]
+                 ))
+              )),
+           []));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "y")), None),
+              (Eif_then_else (
+                 (Ebin_op (Gt,
+                    (Efield_access (
+                       (Efield_access ((Evar (Id "x")), (Label "inner"))),
+                       (Label "x"))),
+                    (Econst (Int 100)))),
+                 (Efun_application ((Evar (Id "print_int")),
+                    (Efield_access (
+                       (Efield_access ((Evar (Id "x")), (Label "inner"))),
+                       (Label "x")))
+                    )),
+                 (Some (Efun_application ((Evar (Id "print_endline")),
+                          (Efield_access ((Evar (Id "x")), (Label "x"))))))
+                 ))
+              )),
+           []))
+        ]
+   
+  |}]
+;;
+
+let%expect_test _ =
+  parse
+    "\n\
+    \      type d = { aa: int}\n\
+    \      type t = { aa: int; bb: bool }\n\
+    \      type nested = {raz: t; dva: d}\n\
+    \      let a = { raz = { aa = 4; bb = true}; dva = {aa = 10} }\n\
+    \      let b = { aa = 100; bb = not false }\n\
+    \      let c = (b.aa + a.raz.aa) / a.dva.aa";
+  [%expect
+    {|
+      type d = { aa : int } ;;
+      type t = { aa : int ; bb : bool } ;;
+      type nested = { raz : t ; dva : d } ;;
+      let  a = { raz = { aa = 4 ; bb = true } ; dva = { aa = 10 } } ;;
+      let  b = { aa = 100 ; bb = not (false) } ;;
+      let  c = (b.aa + a.raz.aa) / a.dva.aa ;;
+      [(SType ("d", (Sfield_decl ((Label "aa"), int)), []));
+        (SType ("t", (Sfield_decl ((Label "aa"), int)),
+           [(Sfield_decl ((Label "bb"), bool))]));
+        (SType ("nested", (Sfield_decl ((Label "raz"), t)),
+           [(Sfield_decl ((Label "dva"), d))]));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "a")), None),
+              (Erecord (
+                 (Erecord_field ((Label "raz"),
+                    (Erecord ((Erecord_field ((Label "aa"), (Econst (Int 4)))),
+                       [(Erecord_field ((Label "bb"), (Econst (Bool true))))]))
+                    )),
+                 [(Erecord_field ((Label "dva"),
+                     (Erecord ((Erecord_field ((Label "aa"), (Econst (Int 10)))),
+                        []))
+                     ))
+                   ]
+                 ))
+              )),
+           []));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "b")), None),
+              (Erecord ((Erecord_field ((Label "aa"), (Econst (Int 100)))),
+                 [(Erecord_field ((Label "bb"),
+                     (Eun_op (Not, (Econst (Bool false))))))
+                   ]
+                 ))
+              )),
+           []));
+        (SValue (Non_recursive,
+           (Evalue_binding (((PVar (Id "c")), None),
+              (Ebin_op (Div,
+                 (Ebin_op (Add, (Efield_access ((Evar (Id "b")), (Label "aa"))),
+                    (Efield_access (
+                       (Efield_access ((Evar (Id "a")), (Label "raz"))),
+                       (Label "aa")))
+                    )),
+                 (Efield_access ((Efield_access ((Evar (Id "a")), (Label "dva"))),
+                    (Label "aa")))
+                 ))
+              )),
+           []))
+        ]
+   
+  |}]
+;;
